eaa01ad189ad7545788abb54bee0bf2685391e90
Mateus Farinelli Zardo <mateusfarinellizardo@gmail.com>
2020-01-14 16:37:35 -0300

    Backend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require ('mongoose')
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect('mongodb+srv://root:geo2020dev@geodev-m3n2b.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true  
+})
+
+app.use(express.json()) // Express passa a entender requisições com formato json.
+app.use(routes)
+
+// GET => quando buscamos alguma informação
+// POST => quando criamos alguma informação
+// PUT => quando alteramos alguma informação
+// DELETE => quando deletamos alguma informação
+
+// Query Params: é acessado através de request.query e são utilizados para Filtros, Ordenação, Paginação...
+// Routes Params: é acessado através de request.param e é utilizado para identificar um recurso na alteração ou remoção
+// Body: é acessado pelo request.body e é utilziado para criação ou atelração de registros
+
+app.listen(3333)
